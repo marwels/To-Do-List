a71bd3f9fc443428742cdfff3b1063d3f25bf524
marwels <mwelfe@gmail.com>
2023-01-05 12:15:01 +0100

    feat: style addProject

--- src/components/addProject.js
+++ src/components/addProject.js
@@ -2,10 +2,10 @@ const addProject = function addProject(targetEl) {
   const addProjectContainer = document.createElement("div");
   addProjectContainer.className = "addProjectContainer";
 
-  const icon = document.createElement("div");
-  icon.className = "material-symbols-outlined iconAddProject";
-  icon.innerText = "menu";
-  addProjectContainer.appendChild(icon);
+  // const icon = document.createElement("div");
+  // icon.className = "material-symbols-outlined iconAddProject";
+  // icon.innerText = "menu";
+  // addProjectContainer.appendChild(icon);
 
   const inputAddProject = document.createElement("input");
   inputAddProject.className = "inputAddProject";
@@ -14,14 +14,16 @@ const addProject = function addProject(targetEl) {
   addProjectContainer.appendChild(inputAddProject);
 
   const buttons = document.createElement("div");
-  buttons.className = "addProjeceButtonsContainer";
+  buttons.className = "addProjetButtonsContainer";
 
   const addButton = document.createElement("button");
   addButton.innerText = "Add";
+  addButton.className = "Add";
   buttons.appendChild(addButton);
 
   const cancelButton = document.createElement("button");
   cancelButton.innerText = "Cancel";
+  cancelButton.className = "Cancel";
   buttons.appendChild(cancelButton);
 
   addProjectContainer.appendChild(buttons);
