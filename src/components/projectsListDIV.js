b2c42cbf766db7172f621ef4ce200c2a8c0af3c9
marwels <mwelfe@gmail.com>
2023-01-12 16:14:25 +0100

    feat: add dropdown menu to delete/rename project

--- src/components/projectsListDIV.js
+++ src/components/projectsListDIV.js
@@ -15,17 +15,51 @@ const projectsListDIV = function projectsListDIV(targetEl, projects) {
     // projectDiv.appendChild(icon);
 
     const projectName = document.createElement("button");
-    projectName.className = "material-symbols-outlined icon";
+    projectName.className = "projectName";
     projectName.innerText = projects[i];
     singleProjectContainer.appendChild(projectName);
 
+    // dropdown menu rename/delete
+    const deleteOrRenameContainer = document.createElement("div");
+    deleteOrRenameContainer.className = "dropdown";
     const deleteOrRename = document.createElement("button");
-    deleteOrRename.className = "material-symbols-outlined deleteOrRename";
+    deleteOrRename.className =
+      "material-symbols-outlined deleteOrRename dropbtn";
     deleteOrRename.innerText = "more_vert";
-    singleProjectContainer.appendChild(deleteOrRename);
+    deleteOrRenameContainer.appendChild(deleteOrRename);
+    const dropdown = document.createElement("div");
+    dropdown.className = "dropdown-content";
+    const dropdownRename = document.createElement("button");
+    dropdownRename.innerText = "Rename";
+    dropdownRename.className = "rename";
+    dropdown.appendChild(dropdownRename);
+    const dropdownDelete = document.createElement("button");
+    dropdownDelete.className = "delete";
+    dropdownDelete.innerText = "Delete";
+    dropdown.appendChild(dropdownDelete);
+    deleteOrRenameContainer.appendChild(dropdown);
+    singleProjectContainer.appendChild(deleteOrRenameContainer);
+
+    deleteOrRename.addEventListener("click", () => {
+      dropdown.classList.toggle("show");
+    });
+
     children.push(singleProjectContainer);
     targetEl.appendChild(singleProjectContainer);
+
+    window.onclick = function (event) {
+      if (!event.target.matches(".dropbtn")) {
+        const dropdowns = document.getElementsByClassName("dropdown-content");
+        for (let i = 0; i < dropdowns.length; i++) {
+          const openDropdown = dropdowns[i];
+          if (openDropdown.classList.contains("show")) {
+            openDropdown.classList.remove("show");
+          }
+        }
+      }
+    };
   }
+
   return () => {
     children.forEach((element) => {
       targetEl.removeChild(element);
