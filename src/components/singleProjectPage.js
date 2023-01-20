1a818a2a6ad5064a7e5e55d0a1e917b301a9aac4
marwels <mwelfe@gmail.com>
2023-01-20 13:34:52 +0100

    feat: add single project page

--- src/components/singleProjectPage.js
+++ src/components/singleProjectPage.js
@@ -1,4 +1,3 @@
-import CreateElement from "./small/CreateElement";
 import AddTaskForm from "./AddTaskForm";
 
 const singleProjectPage = function singleProjectPage(
@@ -8,7 +7,7 @@ const singleProjectPage = function singleProjectPage(
   onAddTask
 ) {
   const singleProjectPageContainer = document.createElement("div");
-  singleProjectPageContainer.className = "showProjectPageContainer";
+  singleProjectPageContainer.className = "singleProjectPageContainer";
   const project = projects.get(projectId);
 
   if (!project) {
@@ -20,12 +19,55 @@ const singleProjectPage = function singleProjectPage(
     pageTitle.className = `pageTitle ${project.name}`;
     pageTitle.innerText = `Project: ${project.name}`;
     singleProjectPageContainer.appendChild(pageTitle);
+
     AddTaskForm(singleProjectPageContainer, project, projectId, onAddTask);
 
-    const listOfToDosDIV = document.createElement("div");
-    listOfToDosDIV.className = "showProjectPageContainer";
-    // tu wszystkie taski
-    singleProjectPageContainer.appendChild(listOfToDosDIV);
+    const listOfTasks = document.createElement("div");
+    listOfTasks.className = "listOfToTasks";
+
+    // all Tasks
+    project.tasks.forEach((task, taskId) => {
+      const singleTaskContainer = document.createElement("div");
+      singleTaskContainer.className = "singleTaskContainer";
+      singleTaskContainer.dataset.taskName = task.name;
+
+      const singleTaskPriority = document.createElement("div");
+      singleTaskPriority.className = "singleTaskNameDiv";
+
+      if (task.priority === 1) {
+        singleTaskPriority.classList.add("green");
+      } else if (task.priority === 2) {
+        singleTaskPriority.classList.add("orange");
+      } else if (task.priority === 3) {
+        singleTaskPriority.classList.add("red");
+      } else {
+        console.log("check priority");
+      }
+      singleTaskContainer.appendChild(singleTaskPriority);
+
+      const singleTaskNameDiv = document.createElement("div");
+      singleTaskNameDiv.className = "singleTaskNameDiv";
+      singleTaskNameDiv.innerText = task.name;
+      singleTaskContainer.appendChild(singleTaskNameDiv);
+
+      const singleTaskDescription = document.createElement("div");
+      singleTaskDescription.className = "singleTaskDescription";
+      singleTaskDescription.innerText = task.description;
+      singleTaskContainer.appendChild(singleTaskDescription);
+
+      const singleTaskDate = document.createElement("div");
+      singleTaskDate.className = "singleTaskDate";
+      singleTaskDate.innerText = task.date;
+      singleTaskContainer.appendChild(singleTaskDate);
+
+      const singleTaskStatus = document.createElement("input");
+      singleTaskStatus.className = "singleTaskStatus";
+      singleTaskStatus.setAttribute("type", "checkbox");
+      singleTaskContainer.appendChild(singleTaskStatus);
+
+      listOfTasks.appendChild(singleTaskContainer);
+    });
+    singleProjectPageContainer.appendChild(listOfTasks);
   }
 
   targetEl.appendChild(singleProjectPageContainer);
